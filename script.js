a83af99c55e38ade7ee847e7dea6be292a3bbe3e
Sa1 <abde88227@gmail.com>
2026-01-30 00:56:17 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+let totalScore = 0;
+const completedTasks = {};
+
+function submitTask(taskId) {
+  const input = document.getElementById(`task${taskId}Input`);
+  const button = input.nextElementSibling;
+
+  if (!input.value || input.value.length < 4) {
+    alert("Please provide a valid value");
+    return;
+  }
+
+  if (completedTasks[taskId]) return;
+
+  completedTasks[taskId] = true;
+  totalScore += 25;
+
+  button.disabled = true;
+  button.innerText = "Completed";
+  input.disabled = true;
+
+  updateScore();
+}
+
+function updateScore() {
+  document.getElementById("scoreValue").innerText = totalScore;
+}
